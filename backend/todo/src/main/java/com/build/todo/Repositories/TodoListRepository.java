d2acffab850f3c6c5899e2d31e8fbb443852f607
OnkarGade <onkargade28@gmail.com>
2025-12-04 19:33:38 +0530

    Delete backend/todo directory

--- backend/todo/src/main/java/com/build/todo/Repositories/TodoListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.build.todo.Repositories;
-
-import com.build.todo.Models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoListRepository extends JpaRepository<Task, Long>  {
-}
